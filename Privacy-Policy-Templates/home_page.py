d700b763cc91918e4c0e2f9ca53a46927be7c51d
TylerK4Brown <tylerk4brown@gmail.com>
2026-04-22 11:20:21 -0400

    Added an area for our privacy policy templates, which will be fed into an AI model to generate actual privacy policies

---
+++ Privacy-Policy-Templates/home_page.py
@@ -0,0 +1,27 @@
+# Page configuration initialization for our privacy policy template 'playground'
+# This will be a small website that we can access to test our privacy policy templates/refine the code of them
+import streamlit as st
+
+# Initialize the page configuration for the application
+st.set_page_config(
+    page_title="AI Privacy Policy Helper"
+)
+# Stretches the text out so that it fits most of the page
+st.set_page_config(layout="wide")
+
+# Page layout
+# Dictionary of lists, each list containing a section name and its corresponding page path.
+# this is displayed at the top of the screen
+pages = {
+    "Introduction": [
+        st.Page("pages/1_Welcome.py", title='Welcome')
+    ],
+    "Privacy Policy Templates": [
+        st.Page("pages/2_Nutrition_Label_Template.py"),
+        st.Page("pages/3_Tabular_Format_Template.py")
+    ]
+}
+
+# Instantiates the navigation so it can be displayed on the landing page
+pg = st.navigation(pages, position='top')
+pg.run()
---
+++ Privacy-Policy-Templates/pages/2_Nutrition_Label_Template.py
@@ -0,0 +1,8 @@
+# Nutrition Label Template page
+# Currently a placeholder, but will eventually contain Streamlit code of a "Nutrition label" format
+# We'll try to recreate something like this: https://mashable.com/article/apple-privacy-nutrition-labels-ios14
+
+import streamlit as st
+
+st.markdown('# Work in progress...')
+st.image("images/sunset.jpg", caption="something to look at for now.", width=400)
