ce0b03f48511a92d5ceafb57353428785174f51a
Lilli <brookslillikb@gmail.com>
2026-04-26 19:15:15 -0400

    Delete 3_Tabular_Format_Template.py

--- 3_Tabular_Format_Template.py
+++
@@ -1,9 +0,0 @@
-# Tabular Format Template page
-# We'll try to recreate the typical financial institution privacy policy tables
-# Something like this would be acceptable: https://www.consumerfinance.gov/rules-policy/regulations/1016/a/#ImageA1b
-# We'll train our LLM to add custom icons/images to these tables so they're less bland
-
-import streamlit as st
-
-st.markdown('# Work in progress...')
-st.image("images/sunset.jpg", caption="something to look at for now.", width=400)
