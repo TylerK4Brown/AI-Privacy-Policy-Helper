c6b5846427729ebe042138c1465b4f21bd136d6d
TylerK4Brown <tylerk4brown@gmail.com>
2026-04-29 12:30:39 -0400

    New Tabular Template created that works with the summary JSON output

--- Privacy-Policy-Templates/pages/3_Tabular_Format_Template.py
+++ Privacy-Policy-Templates/pages/3_Tabular_Format_Template.py
@@ -1,9 +1,52 @@
 # Tabular Format Template page
 # We'll try to recreate the typical financial institution privacy policy tables
 # Something like this would be acceptable: https://www.consumerfinance.gov/rules-policy/regulations/1016/a/#ImageA1b
-# We'll train our LLM to add custom icons/images to these tables so they're less bland
-
 import streamlit as st
 
-st.markdown('# Work in progress...')
-st.image("images/sunset.jpg", caption="something to look at for now.", width=400)
+# Check to see if a user generated a summary yet
+# If not, ask them to go back and generate one
+summary = st.session_state.get("summary", None)
+st.title("Privacy Policy Tabular Format")
+
+if not summary:
+    st.warning("No summary found. Please go back and generate one first.")
+    st.stop()
+
+# Table dictionary to pass into st.table
+# Follows a very similar format to the nutrition label page
+# Main difference lies in its structure, more tabular + less focused on design
+table_format = {
+    "Data Type": [
+        "🔒 Data privacy", 
+        "🔍 Data collection", 
+        "🔗 Third-Party Sharing",
+        "🛡️ Security",
+        "⚖️ User Rights",
+        "⚠️ Key Risks"],
+    
+    "Description": [
+        "Information about how your data is protected and used", 
+        "Information about what data is collected and how it's collected", 
+        "Information shared with third parties",
+        "Information about the security measures in place to protect your data",
+        "Information about your rights regarding your data",
+        "Information about potential risks associated with your data"],
+    
+    "Purpose": [
+        # Data Privacy
+        f'''- {summary.get("data_usage", "Not specified")}''',
+        # Data Collection
+        f'''- {summary.get("data_collection", "Not specified")}''',
+        # Third-Party Sharing
+        f'''- {summary.get("third_party_sharing", "Not specified")}''',
+        # Security
+        f'''- {summary.get("security", "Not specified")}''',
+        # User Rights
+        f'''- {summary.get("user_rights", "Not specified")}''',
+        # Key Risks
+        f'''- {summary.get("key_risks", "Not specified")}''']
+}
+
+st.table(table_format, height='content')
+
+
